8f19b74a00101df5dfd6bd1cee12011b98b9c080
Shemar Dacosta <dshemar6@gmail.com>
2018-11-20 18:04:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
 // add solution here
 // add solution here
- function theBeatlesPlay(musicians, instruments){
-  var content = []
-  for(var i = 0; i < musicians.length; i++){
-    var answer = console.log( musicians[i] +" plays " + instruments[i]);
-    return answer
-    // pushes array contents into  one variable
-    content.push(musicians[i] +" plays " + instruments[i])
-  }
-  return content
+function theBeatlesPlay(musicians, instruments) {
+    var content = []
+    for (var i = 0; i < musicians.length; i++) {
+        var answer = console.log(musicians[i] + " plays " + instruments[i]);
+        return answer
+        // pushes array contents into  one variable
+        content.push(musicians[i] + " plays " + instruments[i])
+    }
+    return content
 }
- function johnLennonFacts(facts){
-  var i = 0
-  var factuals = []
-  while (i < facts.length) {
-    factuals.push(facts[i] += "!!!");
-    i++;
+
+function johnLennonFacts(facts) {
+    var i = 0
+    var factuals = []
+    while (i < facts.length) {
+        factuals.push(facts[i] += "!!!");
+        i++;
+    }
+    return factuals
 }
-return factuals
+
+function iLoveTheBeatles(number) {
+    var content = []
+    do {
+        content.push("I love the Beatles!")
+        number++
+    }
+    while (number < 15) {
+        return content
+    }
+
 }
- function iLoveTheBeatles(number){
-  var content = []
-  do {
-    content.push("I love the Beatles!")
-    number++
-  }
-while (number < 15){
-  return content
-}
-  
-} 
